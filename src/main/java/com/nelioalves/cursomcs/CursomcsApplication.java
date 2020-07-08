7192165effa63be2367325123da2121d71b3eabc
Abbas Atwi <abbas_9589_new@hotmail.com>
2020-07-08 18:25:32 -0300

    Projeto criado

---
+++ src/main/java/com/nelioalves/cursomcs/CursomcsApplication.java
@@ -0,0 +1,13 @@
+package com.nelioalves.cursomcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nelioalves/cursomcs/CursomcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelioalves.cursomcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
